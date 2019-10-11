b92d7d4ab7fbaca37e83a7190f557616fb6357f9
ravi <47279895+ravi-kumar-maury@users.noreply.github.com>
2019-10-11 18:15:46 +0530

    Add files via upload

---
+++ virtual_function.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int main() {
+  /*oops concepts 
+  1.polymorphism 
+  1(a).runtime polymorphism 
+  1(b).compile time polymorphism 
+  2.encapsulation
+  3.inheritance
+concept of virtual function (why would even bother 
+to use 'keyword virtual ')
+*/
+class A 
+{
+
+int x;
+public :
+virtual  void show()
+  {
+     cout<<"hi,i am in class A\n";
+    
+  }
+  void i_love_chess()
+    {
+      cout<<"i_love_chess\n"; 
+    }
+
+};
+class B:public A
+{
+  int y;
+  public :
+  void show()
+   {
+
+       cout<<"hi,i am in class B\n";
+   }
+};
+B b;
+A a;
+//b.show();
+A *aptr;  
+aptr=&a;
+aptr->show();  //accessing class A show
+//exit(0); 
+aptr=&b;
+aptr->show();//without "virtual" keyword in base class "show" function ,this always calls "show function" in base clas despite of overriding                    
+//aptr=&a;
+//cout<<"reference changed\n";
+//aptr->show();
+//a.show();
+//b.i_love_chess();
+
+}
